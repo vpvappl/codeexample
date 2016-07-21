eb0011ed8a6ec727ec5309c4b7eab716440eecba
vpvappl <vpvappl@yandex.ru>
2016-07-21 14:42:33 +0300

    Update yii2_RESTful_controller.php

--- yii2_RESTful_controller.php
+++ yii2_RESTful_controller.php
@@ -1,2 +1,68 @@
 <?php
 
+namespace rest\modules\v1\controllers;
+
+use Yii;
+use yii\rest\ActiveController;
+use rest\modules\v1\models\WpPosts;
+use yii\web\Response;
+use yii\filters\ContentNegotiator;
+
+class ArticlesController extends ActiveController
+{
+    public $modelClass = 'rest\modules\v1\models\WpPosts';
+    
+    /**
+     * Какое API может быть доступно
+     * 1. последние статьи без анонсов
+     * 2. последние статьи с анонсами
+     * 3. любимая статья одна
+     */
+    
+    protected function formatData ($format) 
+    {
+        if ($format == 'json') 
+        {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+        }
+        else 
+        {
+            Yii::$app->response->format = Response::FORMAT_XML;
+        }
+    }
+    
+    public function behaviors()
+    {
+        return [
+            'class' => 'rest\components\LimiterFilter',
+        ];
+    } 
+    
+    public function actionOne($format = 'xml', $link = false) 
+    {
+        $this->formatData($format);
+        $model = new WpPosts();
+        return $model->oneArticle($link);        
+    }    
+    
+    public function actionLastanons($count=10, $format = 'xml') 
+    {
+        $this->formatData($format);
+        $model = new WpPosts();
+        return $model->lastArtAnons();        
+    }    
+    
+    public function actionLast($count=10, $format = 'xml') 
+    {
+        $this->formatData($format);
+        $model = new WpPosts();
+        return $model->lastArt();        
+    }    
+    
+    public function actionNovelty ($count=10, $format = 'xml') 
+    {
+        $this->formatData($format);
+        $model = new WpPosts();
+        return $model->infoShort($count);        
+    }
+}
